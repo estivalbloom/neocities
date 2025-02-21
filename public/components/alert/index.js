9f45a3b01e6c0d9c1656fd5129cfd8e29151f7c8
estivalbloom <estivalbloom@gmail.com>
2025-02-21 16:13:38 -0500

    Refactors custom element definitions

--- public/components/alert/index.js
+++ public/components/alert/index.js
@@ -1,4 +1,4 @@
-async function setup() {
+async function setup(style_src) {
 	let response = await fetch('/components/alert/template.html');
 	let html = await response.text();
 	const parser = new DOMParser()
@@ -26,15 +26,20 @@ async function setup() {
 			const shadow = this.attachShadow({ mode: 'open' });
 			shadow.appendChild(template.content.cloneNode(true));
 
-			const closeButton = shadow.querySelector('#close-button');
-			const okButton = shadow.querySelector('#ok-button');
-			const cancelButton = shadow.querySelector('#cancel-button');
+			const style_link = document.createElement('link');
+			style_link.setAttribute('rel', 'stylesheet');
+			style_link.setAttribute('href', style_src);
+			shadow.appendChild(style_link);
+
+			const close_button = shadow.querySelector('#close-button');
+			const ok_button = shadow.querySelector('#ok-button');
+			const cancel_button = shadow.querySelector('#cancel-button');
 			this._title_text_elem = shadow.querySelector('#title-text');
 
 			// okButton.style.visibility = 'collapse'
 			// cancelButton.style.visibility = 'collapse'
 
-			closeButton.addEventListener('click', e => {
+			close_button.addEventListener('click', e => {
 				if (this.closeable) {
 					this.remove();
 				}
@@ -42,9 +47,9 @@ async function setup() {
 				e.preventDefault();
 			});
 
-			okButton.addEventListener('click', e => this._wrapAndDispatch('okclick', e));
+			ok_button.addEventListener('click', e => this._wrapAndDispatch('okclick', e));
 
-			cancelButton.addEventListener('click', e => this._wrapAndDispatch('cancelclick', e));
+			cancel_button.addEventListener('click', e => this._wrapAndDispatch('cancelclick', e));
 
 			this._isReady = true;
 			for (const [name, value] of Object.entries(this._pendingAttributes)) {
@@ -83,4 +88,4 @@ async function setup() {
 	}
 }
 
-export default await setup()
+export default setup
--- public/components/index.js
+++ public/components/index.js
@@ -1,1 +1,10 @@
-export { default as Alert } from './alert/alert.js'
+const style_src = 'https://unpkg.com/98.css';
+
+import alertSetup from './alert/index.js'
+const Alert = await alertSetup(style_src)
+
+function defineAll() {
+	customElements.define('win-alert', Alert, { extends: 'div' });
+}
+
+export { defineAll, Alert }
